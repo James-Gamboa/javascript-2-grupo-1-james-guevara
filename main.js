9ec26ed2e4b0921e937cd5352cb075bfcc9b1fc8
James <jjguevarag@gmail.com>
2023-01-17 18:37:07 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,107 @@
+// Prueba Diagnostico
+
+// I Parte: Teoría
+
+// 1. Respuesta pregunta 1
+
+// Colocar la respuesta acá
+// Numero(number) - ejemplo 200
+//str (string)- ejemplo "Hola mundo"
+//booleanos(boolean) - ejemplo "true o false"
+//undefined (undefined) - ejemplo "undefined"
+//null (null) - ejemplo "null"
+
+ // ====================================================================================================
+
+// 2. Respuesta pregunta 2
+
+// Colocar la respuesta acá
+//utilizando los corchetes 
+//ejemplo :
+// let myObject  = {};
+// myObject ["nuevapropiedad"] = "8";
+// console.log(myObject ["nuevapropiedad"]); // resultado: 8
+//  otra forma seria utilizando el punto 
+// let object  = {};
+// Object.nuevafuncion = "hola mundo";
+// console.log(Object .nuevafuncion; // resultado: hola mundo
+// ====================================================================================================
+
+
+// 3. Respuesta pregunta 3
+
+// Colocar la respuesta acá
+// lo que hace += en el codigo es que tiene la funcion de sumar un valor de la variable list 
+// -1 el primer console.log el resultado que imprimiria seria 4465
+// -2 el segundo console.log el resultado que imprimiria seria 4465textodeprueba
+// ====================================================================================================
+
+
+// 4. Respuesta pregunta 4
+
+// Colocar la respuesta acá
+// el operador  != hace que si compare dos valores son diferentes entre si 
+// el operador !== hace que compare si dos valores son diferentes entre si y si son del mismo tipó
+// el resultado de a seria true ya que flag es false
+//el resultado de b seria true ya que flag es false es de tipo boleano 
+// ====================================================================================================
+
+
+// 5. Respuesta pregunta 5
+
+// Colocar la respuesta acá
+// una funcion anonima es una funcion sin nombre
+// ====================================================================================================
+
+
+// 6. Respuesta pregunta 6
+// Colocar la respuesta acá
+// la funcion declaration se utiliza cuando son cargadas antes que se ejecute cualquier otro codigo 
+// y la funcion expression son cargadas en el momento en que se ejecuta la linea de codigo donde se encuentra la funcion
+
+// ====================================================================================================
+
+
+// 7. Respuesta pregunta 7
+
+// Colocar la respuesta acá
+// un metodo es una funcion que es parte de un objeto
+// ====================================================================================================
+
+
+// 8. Respuesta pregunta 8
+
+// Colocar la respuesta acá
+// recursividad es que se llama a si misma 
+// ====================================================================================================
+
+
+// II Parte: Ejercicios
+
+// 1. Respuesta pregunta 1
+
+// Colocar el código acá
+// function numeros(numbers) {
+//   let pares = [];
+//   let impares = [];
+
+//   for (let i = 0; i < numbers.length; i++) {
+//       if (numbers[i] % 2 === 0) {
+//           pares.push(numbers[i]);
+//       } else {
+//           impares.push(numbers[i]);
+//       }
+//   }
+//   console.log("numeros pares:", pares);
+//   console.log("numeros impares:", impares);
+// }
+
+// let lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// numeros(lista);
+
+// ====================================================================================================
+
+
+// 2. Respuesta pregunta 2
+
+// ====================================================================================================
